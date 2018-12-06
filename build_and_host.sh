3b6c8d97205033e9ed49cbfa29d287bb50eae27f
Jim Thario <jtthario@amazon.com>
2018-12-05 17:15:11 -0800

    Minor updates for format of S3 origin URL

--- build_and_host.sh
+++ build_and_host.sh
@@ -12,35 +12,46 @@
 ORIGIN=`pwd`
 BUILD=$ORIGIN/build
 
+# date stamp for this deploy
+STAMP=`date +%s`
+echo build stamp is $STAMP
+
+# clear the build folder
 rm -f $BUILD/*
 
-ZIP=speke-reference-lambda.zip
-
+# create the reference server zip with a unique name
+SERVZIP=speke-reference-lambda-$STAMP.zip
 cd $ORIGIN/src
 # using zappa for help in packaging
-zappa package --output $BUILD/$ZIP
+zappa package --output $BUILD/$SERVZIP
 
-ZIP=cloudformation-resources.zip
-
+# create the custom resource zip with a unique name
+RESZIP=cloudformation-resources-$STAMP.zip
 cd $ORIGIN/cloudformation
-cp *.json $BUILD
-zip -r $BUILD/$ZIP mediapackage_endpoint_common.py mediapackage_speke_endpoint.py resource_tools.py
+zip -r $BUILD/$RESZIP mediapackage_endpoint_common.py mediapackage_speke_endpoint.py resource_tools.py
+
+# update templates with the new zip filenames
+sed -e "s/DEV_0_0_0/$STAMP/g" speke_reference.json >$BUILD/speke_reference.json
+sed -e "s/DEV_0_0_0/$STAMP/g" mediapackage_speke_endpoint.json >$BUILD/mediapackage_speke_endpoint.json
 
 cd $BUILD
 
-# regional buckets hosting the pre-built cloudformation templates
+# regional buckets hosting the pre-built cloudformation templates and resources
 
-aws s3 sync . s3://rodeolabz-ap-northeast-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-ap-northeast-2/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-ap-south-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-ap-southeast-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-ap-southeast-2/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-eu-central-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-eu-west-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-eu-west-3/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-sa-east-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-us-east-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-us-west-1/speke/ --acl public-read --delete
-aws s3 sync . s3://rodeolabz-us-west-2/speke/ --acl public-read --delete
+# primary bucket
+aws s3 sync . s3://rodeolabz-us-west-2/speke --acl public-read --delete
+
+# sync'd buckets
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-ap-northeast-1/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-ap-northeast-2/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-ap-south-1/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-ap-southeast-1/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-ap-southeast-2/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-eu-central-1/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-eu-west-1/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-eu-west-3/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-sa-east-1/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-us-east-1/speke --acl public-read --delete
+aws s3 sync s3://rodeolabz-us-west-2/speke s3://rodeolabz-us-west-1/speke --acl public-read --delete
 
 cd $ORIGIN
