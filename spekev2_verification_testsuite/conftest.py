9c0e379686d5dd1d18cb2f034a0f2a553f87bba1
Vinay Calastry <vinaycr@amazon.com>
2022-01-31 13:28:22 -0800

    Update speke reference server test suite for more presets

--- spekev2_verification_testsuite/conftest.py
+++ spekev2_verification_testsuite/conftest.py
@@ -10,14 +10,14 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="session", autouse=True)
 def spekev2_url(request):
     """
-    Example: "https://<api-gateway-id>.execute-api.us-west-2.amazonaws.com/EkeStage/copyProtection"
+    Example: "https://<api-gateway-id>.execute-api.<aws-region>.amazonaws.com/EkeStage/copyProtection"
     """
     return request.config.getoption("--speke-url")
+
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
     date_now = datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S").replace(" ", "_")
     if not os.path.exists('reports'):
         os.makedirs('reports')
-    config.option.htmlpath = 'reports/test_results_'+date_now+".html"
-  
+    config.option.htmlpath = 'reports/test_results_' + date_now + ".html"
--- spekev2_verification_testsuite/helpers/utils.py
+++ spekev2_verification_testsuite/helpers/utils.py
@@ -6,7 +6,6 @@ from io import StringIO
 from collections import Counter
 import m3u8
 import requests
-import pytest
 
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 from aws_requests_auth import boto_utils
@@ -19,11 +18,26 @@ WIDEVINE_PLAYREADY_TEST_FILE = "3_spekev2_dash_widevine_playready_preset_video_1
 WIDEVINE_PSSH_CPD_TEST_FILE = "7_spekev2_dash_widevine_preset_video_1_audio_1_no_rotation_pssh_cpd.xml"
 PLAYREADY_PSSH_CPD_TEST_FILE = "8_spekev2_dash_playready_preset_video_1_audio_1_no_rotation_pssh_cpd.xml"
 PLAYREADY_PSSH_HLSSIGNALINGDATA_TEST_FILE = "9_spekev2_dash_playready_preset_video_1_audio_1_no_rotation_pssh_signallingdata.xml"
-WRONG_VERSION_TEST_FILE = "4_negative_wrong_version_spekev2_dash_widevine.xml" # Wrong CPIX version in request
+WRONG_VERSION_TEST_FILE = "4_negative_wrong_version_spekev2_dash_widevine.xml"  # Wrong CPIX version in request
+
+# TEST CASES
+TEST_CASE_1_P_V_1_A_1 = "test_case_1_p_v_1_a_1"
+TEST_CASE_2_P_V_3_A_2 = "test_case_2_p_v_3_a_2"
+TEST_CASE_3_P_V_5_A_3 = "test_case_3_p_v_5_a_3"
+TEST_CASE_4_P_V_8_A_2 = "test_case_4_p_v_8_a_2"
+
+# PRESET TEST CASES FILE NAMES
+PRESETS_WIDEVINE = "1_widevine.xml"
+PRESETS_PLAYREADY = "2_playready.xml"
+PRESETS_FAIRPLAY = "3_fairplay.xml"
+PRESETS_WIDEVINE_PLAYREADY = "4_widevine_playready.xml"
+PRESETS_WIDEVINE_FAIRPLAY = "5_widevine_fairplay.xml"
+PRESETS_PLAYREADY_FAIRPLAY = "6_playready_fairplay.xml"
+PRESETS_WIDEVINE_PLAYREADY_FAIRPLAY = "7_widevine_playready_fairplay.xml"
 
 SPEKE_V2_REQUEST_HEADERS = {"x-speke-version": "2.0", 'Content-type': 'application/xml'}
 SPEKE_V2_MANDATORY_NAMESPACES = {
-    "cpix" : "urn:dashif:org:cpix", 
+    "cpix": "urn:dashif:org:cpix",
     "pskc": "urn:ietf:params:xml:ns:keyprov:pskc"
 }
 
@@ -67,22 +81,30 @@ SPEKE_V2_GENERIC_RESPONSE_ELEMENT_LIST = [
     '{urn:dashif:org:cpix}CPIX'
 ]
 
-SPEKE_V2_GENERIC_RESPONSE_ATTRIBS_DICT = { 
-                                            'CPIX': ['contentId', 'version'],
-                                            'ContentKey': ['kid', 'commonEncryptionScheme'],
-                                            'DRMSystem': ['kid', 'systemId'],
-                                            'ContentKeyUsageRule': ['kid', 'intendedTrackType']
-                                     }
+SPEKE_V2_GENERIC_RESPONSE_ATTRIBS_DICT = {
+    'CPIX': ['contentId', 'version'],
+    'ContentKey': ['kid', 'commonEncryptionScheme'],
+    'DRMSystem': ['kid', 'systemId'],
+    'ContentKeyUsageRule': ['kid', 'intendedTrackType']
+}
 
 SPEKE_V2_HLS_SIGNALING_DATA_PLAYLIST_MANDATORY_ATTRIBS = ['media', 'master']
 
 ## DRM SYSTEM ID LIST
 WIDEVINE_SYSTEM_ID = 'edef8ba9-79d6-4ace-a3c8-27dcd51d21ed'
 PLAYREADY_SYSTEM_ID = '9a04f079-9840-4286-ab92-e65be0885f95'
+FAIRPLAY_SYSTEM_ID = '94ce86fb-07ff-4f43-adb8-93d2fa968ca2'
 
-def read_xml_file_contents(filename):
-    with open(f"./spekev2_requests/{filename.strip()}", "r") as f:
+HLS_SIGNALING_DATA_KEYFORMAT = {
+    'fairplay': 'com.apple.streamingkeydelivery',
+    'playready': 'com.microsoft.playready'
+}
+
+
+def read_xml_file_contents(test_type, filename):
+    with open(f"./spekev2_requests/{test_type}/{filename.strip()}", "r") as f:
         return f.read().encode('utf-8')
+
 
 def speke_v2_request(speke_url, request_data):
     return requests.post(
@@ -91,6 +113,7 @@ def speke_v2_request(speke_url, request_data):
         data=request_data,
         headers=SPEKE_V2_REQUEST_HEADERS
     )
+
 
 def get_aws_auth(url):
     api_gateway_netloc = urlparse(url).netloc
@@ -106,6 +129,13 @@ def get_aws_auth(url):
         **boto_utils.get_credentials()
     )
 
+
+def send_speke_request(test_xml_folder, test_xml_file, spekev2_url):
+    test_request_data = read_xml_file_contents(test_xml_folder, test_xml_file)
+    response = speke_v2_request(spekev2_url, test_request_data)
+    return response.text
+
+
 def count_tags(xml_content):
     xml_tags = []
     for element in ET.iterparse(StringIO(xml_content)):
@@ -119,6 +149,7 @@ def count_tags(xml_content):
     xml_dict = dict(zip(xml_keys, xml_values))
     return xml_dict
 
+
 def count_child_element_tags_for_element(parent_element):
     xml_tags = [element.tag for element in parent_element]
     xml_keys = Counter(xml_tags).keys()
@@ -126,13 +157,21 @@ def count_child_element_tags_for_element(parent_element):
     xml_dict = dict(zip(xml_keys, xml_values))
     return xml_dict
 
+
+def count_child_element_tags_in_parent(root_cpix, parent_element, child_element):
+    parent_element_xml = root_cpix.find(parent_element)
+    return len(parent_element_xml.findall(child_element))
+
+
 def parse_ext_x_key_contents(text_in_bytes):
     decoded_text = decode_b64_bytes(text_in_bytes)
     return m3u8.loads(decoded_text)
 
+
 def parse_ext_x_session_key_contents(text_in_bytes):
     decoded_text = decode_b64_bytes(text_in_bytes).replace("#EXT-X-SESSION-KEY:METHOD", "#EXT-X-KEY:METHOD")
     return m3u8.loads(decoded_text)
+
 
 def decode_b64_bytes(text_in_bytes):
     return base64.b64decode(text_in_bytes).decode('utf-8')
